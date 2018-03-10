839900633fa78a9259645464054fcd3f6f0ec17f
Test <test@VBU-16.04>
2018-03-10 15:24:45 -0500

    modified port for localhost

--- serve.js
+++ serve.js
@@ -3,7 +3,7 @@ var app = express();
 var path = require('path');
 const PORT = process.env.PORT || 5000
 app.use(express.static(__dirname));
-// viewed at http://localhost:8080
+// viewed at http://localhost:5000
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
