1eec692f1080bd35d1f56b90f7cbdbadfd370c20
Quinn Ciccoretti <quinnc423@gmail.com>
2018-03-15 15:45:12 -0400

    removed duplicated ctrlr

--- js/controllers/replace.sh
+++ js/controllers/replace.sh
@@ -1,6 +1,5 @@
 find . -name '*.js' -exec sed -i 's/SlingshotViveController/SlingshotController/g' {} \;
-find . -name '*.js' -exec sed -i 's/SlingshotViveController/SlingshotController/g' {} \;
-find . -name '*.js' -exec sed -i 's/SlingshotViveController/SlingshotController/g' {} \;
-find . -name '*.js' -exec sed -i 's/SlingshotViveController/SlingshotController/g' {} \;
-find . -name '*.js' -exec sed -i 's/SlingshotViveController/SlingshotController/g' {} \;
-find . -name '*.js' -exec sed -i 's/SlingshotViveController/SlingshotController/g' {} \;
+find . -name '*.js' -exec sed -i 's/BasicViveController/BasicController/g' {} \;
+find . -name '*.js' -exec sed -i 's/DragViveController/DragController/g' {} \;
+find . -name '*.js' -exec sed -i 's/MoveViveController/MoveController/g' {} \;
+find . -name '*.js' -exec sed -i 's/PaintViveController/PaintController/g' {} \;
