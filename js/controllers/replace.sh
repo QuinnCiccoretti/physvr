beb2d7768fe3742aeda9dd01669554de38a1561e
Quinn Ciccoretti <quinnc423@gmail.com>
2018-03-15 15:42:16 -0400

    changed filenames

---
+++ js/controllers/replace.sh
@@ -0,0 +1,6 @@
+find . -name '*.js' -exec sed -i 's/SlingshotViveController/SlingshotController/g' {} \;
+find . -name '*.js' -exec sed -i 's/SlingshotViveController/SlingshotController/g' {} \;
+find . -name '*.js' -exec sed -i 's/SlingshotViveController/SlingshotController/g' {} \;
+find . -name '*.js' -exec sed -i 's/SlingshotViveController/SlingshotController/g' {} \;
+find . -name '*.js' -exec sed -i 's/SlingshotViveController/SlingshotController/g' {} \;
+find . -name '*.js' -exec sed -i 's/SlingshotViveController/SlingshotController/g' {} \;
