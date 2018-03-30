ff492747b88912dc6e6f703c6ef5121fef35e875
Quinn Ciccoretti <quinnc423@gmail.com>
2018-03-30 08:57:52 -0400

    fixed ids for ctrlr 1 and 2

--- js/controllers/BasicController.js
+++ js/controllers/BasicController.js
@@ -4,6 +4,7 @@
 THREE.BasicController = function ( id ) {
 
 	THREE.ViveController.call( this, id );
+	console.log("BasicController instantiated with id:"+id);
 	//UI, appears on the touchpad
 	var geometry = new THREE.CircleGeometry( 1, 32 );
 	var material = new THREE.MeshBasicMaterial( { color:"#ff00ff" } );
@@ -12,6 +13,12 @@ THREE.BasicController = function ( id ) {
 	ui.rotation.x = - 1.45;
 	ui.scale.setScalar( 0.02 );
 	this.add( ui );
+	/**
+	* Returns controller id, 1 or 0
+	*/
+	this.get_id = function() {
+		return id;
+	}
 	/**
 	* The basic controller only updates its position
 	*/
