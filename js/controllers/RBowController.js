2656937f578d2801e68542ed43e2574c0d977d63
Quinn Ciccoretti <quinnc423@gmail.com>
2018-04-19 15:58:42 -0400

    still working on bow

--- js/controllers/RBowController.js
+++ js/controllers/RBowController.js
@@ -1,13 +1,13 @@
 /**
  * @author quinnciccoretti
  */
+var arrow;
 
 THREE.RBowController = function ( id ) {
 	
 	THREE.BasicController.call( this, id, "#703101" );
 	//ui - appears on touchpad
-	var arrow;
-
+	
 
 	this.handle_update = function() {
 		this.update(); //refreshes controller data
@@ -17,11 +17,11 @@ THREE.RBowController = function ( id ) {
 				var mypos = this.get_absolute_position();
 				arrow.position.set(mypos.x,mypos.y,mypos.z);
 				arrow.__dirtyPosition = true;
-				arrow.rotation.applyQuaternion(calculate_quat(mypos, controller1.get_absolute_position() ))
+				arrow.rotation.setFromQuaternion(calculate_quat(controller1.get_absolute_position().clone(), mypos.clone()));
 
 				var gp = this.getGamepad();
 				var d = this.get_absolute_position().distanceTo(controller1.get_absolute_position());
-				this.pulse(d,25);
+				this.pulse(d/5,1);
 			}
 			
 		}
@@ -29,23 +29,17 @@ THREE.RBowController = function ( id ) {
 	
 
 	function onTriggerUp(){
-
-		
-		
 		var diff = this.get_absolute_position().sub(controller1.get_absolute_position());
-		
-
-
-		cyl.setLinearVelocity(diff.multiplyScalar(-10));
+		arrow.setLinearVelocity(diff.multiplyScalar(-30));
 	}
 	function onTriggerDown(){
-		var cyl  = new Physijs.CylinderMesh(
+		arrow = new Physijs.CylinderMesh(
 	    new THREE.CylinderGeometry( .01, .01, .5, 32 ),
-	    new THREE.MeshBasicMaterial({ color: 0x703101 })
+	    new THREE.MeshBasicMaterial({ color: 0x703101 }),
+	    .5	//mass
 		);
-		cyl.material.side = THREE.DoubleSide;
+		arrow.material.side = THREE.DoubleSide;
 
-		arrow = cyl;
 		var mypos = this.get_absolute_position();
 		arrow.position.set(mypos.x,mypos.y,mypos.z);
 		arrow.__dirtyPosition = true;
@@ -54,11 +48,13 @@ THREE.RBowController = function ( id ) {
 
 	function calculate_quat(a,b){
 		// theta = arccos ( (a.b) / a.length *b.length )
+		a.normalize();
+		b.normalize();
 		var theta = Math.acos( a.dot(b) / (a.length()*b.length()));
 		var c = a.cross(b);
 
 		var quaternion = new THREE.Quaternion();
-		quaternion.setFromAxisAngle( c, theta );
+		quaternion.setFromAxisAngle( c, theta * 8);
 		return quaternion;
 	}
 	
