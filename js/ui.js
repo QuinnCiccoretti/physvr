09c7a27427606c7b37f1d4cd18a8b085bedcdc34
Quinn Ciccoretti <quinnc423@gmail.com>
2018-04-21 21:40:37 -0400

    added method for adding text. hello there!

--- js/ui.js
+++ js/ui.js
@@ -1,20 +1,31 @@
-
-function create_text_geometry(text){
+var uifont;
+function init_font_loader(){
 	var loader = new THREE.FontLoader();
 	var geometry;
 	loader.load( 'res/VT323_Regular.json', function ( font ) {
 		//font is loaded
-		geometry = new THREE.TextGeometry( text, {
-			font: font,
-			size: 80,
-			height: 5,
-			curveSegments: 12,
-			bevelEnabled: true,
-			bevelThickness: 10,
-			bevelSize: 8,
-			bevelSegments: 5
-		} );
-		return geometry;
+		uifont = font;
+		//
+		console.log(uifont);
+		
+	});
+}
+function create_text_mesh(text){
+	var text_geometry = create_text_geometry(text);
+	var text_material = new THREE.MeshBasicMaterial({color:"#ff0000"});
+	var text_mesh = new THREE.Mesh( text_geometry, text_material);
+	//var text_scale = 1;
+	//text_mesh.scale.set(text_scale,text_scale,text_scale);
+	// text_mesh.rotation.z = 3.14;
+	return text_mesh;
+}
+function create_text_geometry(text){
+	var text_geom = new THREE.TextGeometry( text, {
+		font: uifont,	//the already loaded font
+		size: 0.05,
+		height: 0.01,
+		curveSegments: 2,
+		bevelEnabled: false,
 	} );
-	
+	return text_geom;
 }
